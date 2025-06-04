365643deced435d3988043706f301658044bb99d
mauriciok49 <mauriciocerqueirak49@gmail.com>
2025-06-03 21:39:01 -0300

    atualizei a lista de exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.1
+
+if (vel > 60) {
+    console.log(`voce esta dirigindo a ${vel} km, acima da velocidade maxima`)
+}
+console.log(`dirrija com cinto de seguranca`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var vel = 59
+
+if (vel > 60) {
+    console.log(`voce esta dirigindo a ${vel} km, acima da velocidade maxima`)
+}else {
+    console.log(`voce esta na velocidade permitida`)
+}
