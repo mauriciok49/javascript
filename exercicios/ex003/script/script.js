6e49f2782e71aa1d91d54b3e3df774b990dd3ae4
mauriciok49 <mauriciocerqueirak49@gmail.com>
2025-06-17 17:03:59 -0300

    atualizei o script.js

--- exercicios/ex003/script/script.js
+++ exercicios/ex003/script/script.js
@@ -1,47 +1,44 @@
-function verificaridade() {
-            let anoAtual = new Date().getFullYear();
-            var fano = document.getElementById("txtano")
+function contar() {
+            var inicio = document.getElementById("ninicio")
+            var fim = document.getElementById("nfim")
+            var passo = document.getElementById("npasso")
             var resultado = document.getElementById("resultado");
-            if (fano.value.length == 0 || fano.value > anoAtual) {
-                window.alert('[erro] Verifique os dados e tente novamente!')
-            }else {
-                var fsex = document.getElementsByName("sexo")
-                var idade = anoAtual - Number(fano.value)
-                var genero = ''
-                var img = document.createElement('img')
-                img.setAttribute('id', 'imagem')
-                if (fsex[0].checked) {
-                    genero = 'homem'
-                    if (idade >= 0 && idade < 10) {
-                        img.setAttribute('src', 'imagens/childman.png')
-                    }else if (idade < 21) {
-                        img.setAttribute('src', 'imagens/youngman.png')
-                    }else if (idade > 21 && idade < 50) {
-                        img.setAttribute('src', 'imagens/midleman.png')
-                    }else {
-                        img.setAttribute('src', 'imagens/oldman.png')
-                    }
 
-                } else if (fsex[1].checked) {
-                    genero = 'mulher'
-                    if (idade >= 0 && idade < 10) {
-                        img.setAttribute('src', 'imagens/childwoman.png')
-                    }else if (idade < 21) {
-                        img.setAttribute('src', 'imagens/youngwoman.png')
-                    }else if (idade > 21 && idade < 50) {
-                        img.setAttribute('src', 'imagens/midlewoman.png')
-                    }else {
-                        img.setAttribute('src', 'imagens/oldwoman.png')
-                    }
-                } else {
-                    window.alert('[erro] Verifique os dados e tente novamente!')
-                }
+            // Verifica se algum campo está vazio
+            if (inicio.value.length === 0 || fim.value.length === 0 || passo.value.length === 0) {
+                window.alert("Impossível contar! Preencha todos os campos.");
             }
-            resultado.style.textAlign = 'center';
-            resultado.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-            resultado.appendChild(img);
-            img.style.margin = 'auto';
-            img.style.marginTop = '10px';
-        
+            else {
+             resultado.innerHTML = "Contando: <br>";
+             let i = Number(inicio.value);
+             let f = Number(fim.value);
+             let p = Number(passo.value);
+            if (p <= 0) {
+                window.alert("passo inválido! considerando passo 1");
+                p = 1;
+            }
+             if( i < f) {
+                //contagem crescente
+                for (let c = i; c <= f; c += p) {
+                resultado.innerHTML += `${c} \u{1F449} `;
+            }
 
-        }
+            resultado.innerHTML += `\u{1F3C1}`;
+
+             }
+             else {
+                //contagem decrescente
+                for (let c = i; c >= f; c -= p ) {
+                    resultado.innerHTML += `${c} \u{1F449} `;
+                }
+                resultado.innerHTML += `\u{1F3C1}`;
+             }
+
+
+            
+
+            }
+
+            
+
+}
