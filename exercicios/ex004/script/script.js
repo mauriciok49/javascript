49949772655f84658fdaae6f0c2e310c6be1b8cc
mauriciok49 <mauriciocerqueirak49@gmail.com>
2025-06-19 12:58:10 -0300

    atualizei o script.js

--- exercicios/ex004/script/script.js
+++ exercicios/ex004/script/script.js
@@ -1,47 +1,25 @@
-function verificaridade() {
-            let anoAtual = new Date().getFullYear();
-            var fano = document.getElementById("txtano")
-            var resultado = document.getElementById("resultado");
-            if (fano.value.length == 0 || fano.value > anoAtual) {
-                window.alert('[erro] Verifique os dados e tente novamente!')
-            }else {
-                var fsex = document.getElementsByName("sexo")
-                var idade = anoAtual - Number(fano.value)
-                var genero = ''
-                var img = document.createElement('img')
-                img.setAttribute('id', 'imagem')
-                if (fsex[0].checked) {
-                    genero = 'homem'
-                    if (idade >= 0 && idade < 10) {
-                        img.setAttribute('src', 'imagens/childman.png')
-                    }else if (idade < 21) {
-                        img.setAttribute('src', 'imagens/youngman.png')
-                    }else if (idade > 21 && idade < 50) {
-                        img.setAttribute('src', 'imagens/midleman.png')
-                    }else {
-                        img.setAttribute('src', 'imagens/oldman.png')
-                    }
-
-                } else if (fsex[1].checked) {
-                    genero = 'mulher'
-                    if (idade >= 0 && idade < 10) {
-                        img.setAttribute('src', 'imagens/childwoman.png')
-                    }else if (idade < 21) {
-                        img.setAttribute('src', 'imagens/youngwoman.png')
-                    }else if (idade > 21 && idade < 50) {
-                        img.setAttribute('src', 'imagens/midlewoman.png')
-                    }else {
-                        img.setAttribute('src', 'imagens/oldwoman.png')
-                    }
-                } else {
-                    window.alert('[erro] Verifique os dados e tente novamente!')
-                }
-            }
-            resultado.style.textAlign = 'center';
-            resultado.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-            resultado.appendChild(img);
-            img.style.margin = 'auto';
-            img.style.marginTop = '10px';
+function tabuada() {
+    let num = document.getElementById('num');
+    let tab = document.getElementById('seltab');
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número entre 1 e 10');
         
-
+    } else {
+        let n = Number(num.value);
+        let c = 1;
+        tab.innerHTML = ''
+        while (c <= 10 ) {
+            //cria um elemento option dentro do select do html
+            let item = document.createElement('option')
+            //adiciona os valores
+            item.text = `${n} x ${c} = ${n*c}`
+            //coloca o valor de cada respectiovo tab
+            item.value = `tab${c}`
+            //adiciona um elemento filho dentro do select, que é o item(option)
+            tab.appendChild(item)
+            //incrementa os elementos do loop
+            c++
         }
+    }
+    
+}
