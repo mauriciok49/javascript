5be96fb06d562130636477f200e142f80f743b27
mauriciok49 <mauriciocerqueirak49@gmail.com>
2025-06-19 12:57:33 -0300

    criei o ambient.js

---
+++ aula15/ambient.js
@@ -0,0 +1,5 @@
+let num = [5, 8 , 2, 9, 3]
+num.sort( )
+console.log(`os valores do vetor sao ${num}`)
+console.log(`o primeiro valor do vetor é ${num[0]}`)
+console.log(`o comprimento do vetor é de ${num.length} posições`)
