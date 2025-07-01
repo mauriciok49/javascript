d69bd71989953ec6c539a222b1d9652918107f38
mauriciok49 <mauriciocerqueirak49@gmail.com>
2025-07-01 18:14:34 -0300

    atualizei o ambient.js

--- aula15/ambient.js
+++ aula15/ambient.js
@@ -3,3 +3,10 @@ num.sort( )
 console.log(`os valores do vetor sao ${num}`)
 console.log(`o primeiro valor do vetor é ${num[0]}`)
 console.log(`o comprimento do vetor é de ${num.length} posições`)
+let pos = num.indexOf(9)
+if (pos === -1) {
+    console.log(`o valor nao foi encontrado`)
+} else {
+    console.log(`o valor 8 esta na posicao ${pos} `)
+
+}
